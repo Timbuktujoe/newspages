c686a3ae973da0cf075d6ccbd07972903b62fd70
Timbuktujoe <23214407+Timbuktujoe@users.noreply.github.com>
2021-12-13 21:34:00 +0100

    working searchbar

--- src/App.js
+++ src/App.js
@@ -2,16 +2,50 @@ import './App.css';
 import { useEffect, useState } from "react"
 
 function App() {
-  const [stories, setStories] = useState()
+
+  const [userInput, setUserInput] = useState("");
+  const [stories, setStories] = useState([])
+
+
+
   useEffect(() => {
-    fetch("https://hn.algolia.com/api")
+    getResults()
+  }, [])
+
+  const updateUserInput = (event) => {
+    setUserInput(event.target.value)
+    console.log(event)
+  }
+
+  const getResults = () => {
+    fetch("https://hn.algolia.com/api/v1/search?query=" + userInput)
       .then((res) => res.json())
-      .then((res) => setStories)
-  })
+      .then((res) => setStories(res.hits))
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    getResults()
+  }
+
 
   return (
     <div className="App">
-      <h1>hello this is a news website</h1>
+      <h1>hello this is a website about: </h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={updateUserInput}
+          type="text"
+          value={userInput}
+          placeholder="what are you looking for..?">
+        </input>
+      </form>
+      {stories
+        .map((story) =>
+          <li>
+            <a href={story.url}>{story.title}</a>
+          </li>
+        )}
     </div>
   );
 }
